fcb94cf733516aaf0e1dd5dc82d93bc40b6b1321
Pedro de Medeiros <pedro.medeiros@gmail.com>
2018-07-24 17:42:44 -0300

    Cria campos necessários no Singleton.

--- singleton.py
+++ singleton.py
@@ -5,8 +5,12 @@ from PyQt5.QtCore import QObject, pyqtSlot, pyqtProperty
 class Singleton(QObject):
     __instance = None
 
+
     def __init__(self, parent = None):
         super().__init__(parent)
+        self.url = "http://geocodeapi.codeplan.df.gov.br"
+        self.colunas = []
+
 
     @classmethod
     def getInstance(cls, *args):
@@ -14,11 +18,23 @@ class Singleton(QObject):
             cls.__instance = cls()
         return cls.__instance
 
-    @pyqtProperty('QString')
-    def x(self):
-        return "x"
 
-    @pyqtSlot(int, int, int)
-    def xpto(self, *args):
-        print("xpto called: " + str(args))
+    @pyqtProperty('QString')
+    def url(self):
+        return self.url
+
+
+    @pyqtSlot(str)
+    def setUrl(self, url):
+        self.url = url
+
+
+    @pyqtSlot(str)
+    def adicionar_coluna(coluna):
+        self.colunas.append(coluna)
+
+
+    @pyqtSlot(str)
+    def remover_coluna(coluna):
+        self.colunas.remove(coluna)
 
